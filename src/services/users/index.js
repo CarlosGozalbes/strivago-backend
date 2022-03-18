8644b97d9aade8f26c250098bdcf1a2b9bf7ecf2
yonatanski <phpcodeyg@gmail.com>
2022-03-18 12:09:19 +0100

    GET,PUT,DELETE by id is DONE(Working fine)

--- src/services/users/index.js
+++ src/services/users/index.js
@@ -23,6 +23,7 @@ userRouter.get("/", async (req, res, next) => {
       res.send(user)
     } else {
       //   next(createError(404, "users not found"))
+      console.log(error)
     }
   } catch (error) {
     console.log(error)
@@ -30,27 +31,38 @@ userRouter.get("/", async (req, res, next) => {
   }
 })
 // --------------------3 get with id-----------------
-userRouter.get("/userId", async (req, res, next) => {
+userRouter.get("/:userId", async (req, res, next) => {
   try {
+    const user = await UserModel.findById(req.params.userId)
+    if (user) {
+      res.send(user)
+    } else {
+      //   next(createError(404, "users not found"))
+      console.log(error)
+    }
   } catch (error) {
     console.log(error)
-    next()
+    next(error)
   }
 })
 // --------------------4 put with id-----------------
 userRouter.put("/:userId", async (req, res, next) => {
   try {
+    const user = await UserModel.findByIdAndUpdate(req.params.userId, req.body, { new: true })
+    res.send(user)
   } catch (error) {
     console.log(error)
-    next()
+    next(error)
   }
 })
 // --------------------5 delete with id-----------------
-userRouter.delete("/userId", async (req, res, next) => {
+userRouter.delete("/:userId", async (req, res, next) => {
   try {
+    const user = await UserModel.findByIdAndDelete(req.params.userId)
+    res.send({ MESSAGE: "user delted from the DB!!", USER_id: user._id })
   } catch (error) {
     console.log(error)
-    next()
+    next(error)
   }
 })
 
